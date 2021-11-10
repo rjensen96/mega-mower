342d9a6bc6b8e0846fbe72cc58b467ab636c7bbd
Ryan Jensen <50556584+rjensen96@users.noreply.github.com>
2021-11-10 15:52:10 -0700

    added comments to main file and tweaked small things

--- arduino/mega-mower/mega-mower.ino
+++ arduino/mega-mower/mega-mower.ino
@@ -12,13 +12,14 @@ void setup() {
   // Declare pins as output:
   pinMode(stepPin, OUTPUT);
   pinMode(dirPin, OUTPUT);
+  Serial.begin(115200);
   Serial.println("hey I made it to setup");
   
   // Set the spinning direction CW/CCW:
 //  digitalWrite(dirPin, HIGH);
 
   // send pulse forward
-  for(int i = 0; i < 800; i++) {
+  for(int i = 0; i < 400; i++) {
     delay(10);
     sendPulse(true);
   }
@@ -26,7 +27,7 @@ void setup() {
   delay(1000);
   
   // rotate back
-  for(int i = 0; i < 800; i++) {
+  for(int i = 0; i < 400; i++) {
     delay(10);
     sendPulse(false);
   }
@@ -36,11 +37,28 @@ void sendPulse(bool forward) {
   digitalWrite(dirPin, forward ? 1 : 0);
   
   digitalWrite(stepPin, HIGH);
-  delayMicroseconds(800);
+  delayMicroseconds(600);
   digitalWrite(stepPin, LOW);
-  delayMicroseconds(800); 
+  delayMicroseconds(600); 
 }
 
 void loop() {
+  Serial.println("We're here in the main loop!");
+
+  /*            ^
+   *    o     < X >   <- arrow keypad controls DIRECTION LEFT/RIGHT
+   *    |       V
+   *  Joystick: controls MAGNITUDE of speed and FORWARD/BACK.
+   *  
+   */
+
+   /*
+    * EACH FRAME IN HERE:
+    * Determine forward/back direction (or zero) -> set pulse rate
+    * Determine right/left direction -> set # of pulses to send each motor
+    * If turning LEFT, send RIGHT [5?] and LEFT[1?] ... some ratio.
+    * Opposite for turning RIGHT.
+    */
   
+  delay(4000);
 }
